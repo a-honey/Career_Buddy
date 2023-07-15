37b1f782ae6325196fede0dee715f0bb33099731
a-honey <jungahyeon0512@khu.ac.kr>
2023-07-15 22:36:52 +0900

    services/api 데이터 json 변경 및 수정

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -4,19 +4,22 @@ const backendPortNumber = "5001";
 const serverUrl =
   "http://" + window.location.hostname + ":" + backendPortNumber;
 
-// userId의 FieldName 데이터 요청하기
+// 각 필드 대장 컴포넌트에서 userId의 FieldName 데이터 요청하기
 const getDatas = async (userId, FieldName) => {
-  try {
-    return await axios.get(`${serverUrl}/${userId}/${FieldName}`, {
-      headers: {
-        Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
-      },
-    });
-  } catch (err) {
-    console.log(`getData로 ${userId}의 ${FieldName}의 데이터들  가져오기 실패`);
-  }
+  console.log(
+    `%cGET 요청: ${serverUrl}/${userId}/${FieldName}`,
+    "color: #a25cd1;"
+  );
+
+  return axios.get(`${serverUrl}/${userId}/${FieldName}`, {
+    // JWT 토큰을 헤더에 담아 백엔드 서버에 보냄.
+    headers: {
+      Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
+    },
+  });
 };
 
+/*
 // FieldName의 documentId 데이터 불러오기
 const getData = async (documentId, FieldName) => {
   try {
@@ -29,46 +32,53 @@ const getData = async (documentId, FieldName) => {
     console.log(`${FieldName}의 ${documentId} 포스트 가져오기 실패`);
   }
 };
+*/
 
 // FieldName에 newData 추가하기
 const addData = async (userId, FieldName, newData) => {
-  try {
-    return await axios.post(`${serverUrl}/${userId}/${FieldName}`, newData, {
-      headers: {
-        Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
-      },
-    });
-  } catch {
-    alert("포스트 업로드 실패");
-  }
+  const bodyData = JSON.stringify(newData);
+
+  console.log(
+    `%cPOST 요청: ${serverUrl}/${userId}/${FieldName}`,
+    "color: #296aba;"
+  );
+  console.log(`%cPOST 요청 데이터: ${bodyData}`, "color: #296aba;");
+
+  return axios.post(`${serverUrl}/${userId}/${FieldName}`, bodyData, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
+    },
+  });
 };
 
 // userId의 FieldName 필드에 data 업데이트하기
 const updateData = async (userId, FieldName, updateData) => {
-  try {
-    return await axios.put(`${serverUrl}/${userId}/${FieldName}`, updateData, {
-      headers: {
-        Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
-      },
-    });
-  } catch {
-    console.log("포스트 업로드 실패");
-  }
+  const bodyData = JSON.stringify(updateData);
+
+  console.log(
+    `%cPUT 요청: ${serverUrl}/${userId}/${FieldName}`,
+    "color: #059c4b;"
+  );
+  console.log(`%cPUT 요청 데이터: ${bodyData}`, "color: #059c4b;");
+
+  return axios.put(`${serverUrl}/${userId}/${FieldName}`, bodyData, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
+    },
+  });
 };
 
 // userId의 FieldName 필드에 data 삭제하기
 const deleteData = async (documentId, FieldName) => {
-  try {
-    await axios.delete(`${serverUrl}/${FieldName}/${documentId}`, {
-      headers: {
-        Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
-      },
-    });
-  } catch {
-    console.log("포스트 삭제 실패");
-  }
+  axios.delete(`${serverUrl}/${FieldName}/${documentId}`, {
+    headers: {
+      Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
+    },
+  });
 };
 
 // tryCatch를 거쳐가도 될듯
 
-export { getData, addData, updateData, deleteData, getDatas };
+export { addData, updateData, deleteData, getDatas };
