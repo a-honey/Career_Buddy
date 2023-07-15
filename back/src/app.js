f2119ae59bc55482752fbf71c3b631bf216dd31b
[AI8/1반] 지영은 <utael706@gmail.com>
2023-07-15 05:09:43 +0000

    Update app.js 라우터 부분에 HEAD 있던 것 삭제

--- back/src/app.js
+++ back/src/app.js
@@ -23,16 +23,13 @@ app.get("/", (req, res) => {
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use(userAuthRouter);
-<<<<<<< HEAD
 app.use(certificateRouter)
 app.use(express.urlencoded({
   extended:true
 }))
 app.use(express.json())
-=======
 app.use(educationRouter);
 
->>>>>>> eduMR
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
 
