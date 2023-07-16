ec79a80f853f84e4756145a0b921d0714fdaf7b3
[AI8/1반] 황동운 <hwangdongwun12@gmail.com>
2023-07-16 09:31:39 +0000

    Update app.js

--- back/src/app.js
+++ back/src/app.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express from "express";
 import { userAuthRouter } from "./routers/userRouter";
-import { ProjectRouter } from "./routers/ProjectRouter";
+
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
@@ -22,11 +22,7 @@ app.get("/", (req, res) => {
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use(userAuthRouter);
-try{
-app.use(ProjectRouter);
-}catch(err){
-  console.log("oh no");
-}
+
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
 
