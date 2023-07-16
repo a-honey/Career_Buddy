a726e8879215a09de60adedb5afe51858d3b177a
[AI8/1반] 황동운 <hwangdongwun12@gmail.com>
2023-07-16 06:45:51 +0000

    Update index.js

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
 
-import { Project } from "./models/Project";
 
 const DB_URL =
-  "mongodb://localhost:27017" ;
-
+  process.env.MONGODB_URL ||
+  "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요.";
   
-  //||"MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요." 
 mongoose.connect(DB_URL);
 const db = mongoose.connection;
 
