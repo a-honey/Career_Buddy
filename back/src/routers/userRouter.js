3fa45bee1ba04d1d74ac9cf65bd1893bf6314941
[AI8/1반] 김준섭 <elice.kdt@gmail.com>
2023-07-20 10:29:11 +0900

    프로필 사진 업로드 기능 완성

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -6,6 +6,9 @@ import { userAuthService } from "../services/userService";
 import { UserModel } from "../db";
 
 const path = require('path');
+
+// multer storage에 있는 파일을 지우는데 사용하기 위해 fs.unlink를 promisify() 합니다.
+// Node 10.23.1 버전부터는 아래의 형식을 지원합니다.
 const fs = require('fs');
 const multer = require('multer');
 const randomstring = require("randomstring");
@@ -13,13 +16,13 @@ const randomstring = require("randomstring");
 // multer를 설정합니다.
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // path 모듈을 활용해 절대 경로를 기반으로 추가적인 상대 경로를 조합해주어야 합니다.
-    // 현재 working directory는 /back/src/routers/ 입니다.
-    // working directory의 절대 경로에다가 ../uploads/ 라는 상대 경로를 추가해줍니다.
+    // [주의] path 모듈을 활용해 절대 경로를 기반으로 추가적인 상대 경로를 조합해주어야 합니다.
+    //       - 현재 working directory는 /back/src/routers/ 입니다.
+    //       - working directory의 절대 경로에다가 ../uploads/ 라는 상대 경로를 추가해줍니다.
     cb(null, path.join(__dirname, '../uploads'))
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + randomstring.generate() + ".jpg")
+    cb(null, file.fieldname + "_" + randomstring.generate() + ".jpg")
   }
 })
 
@@ -271,7 +274,7 @@ userAuthRouter.put("/user/resetpassword", routeSanitizer, async function (req, r
 });
 
 
-// 현재 로그인한 사용자가 입력한 프로필 사진을 데이터베이스 업로드하고, 해당 사진을 base64 문자열과 MIME type으로 프론트엔드에 보내줍니다.
+// 현재 로그인한 사용자가 입력한 프로필 사진을 데이터베이스 업로드하고, 해당 사진의 base64 문자열과 MIME type으로 프론트엔드에 보내줍니다.
 userAuthRouter.post("/user/:user_id/fileupload", upload.single('file'), login_required, async function (req, res, next) {
   try {
     const currentUserId = req.currentUserId;
@@ -280,28 +283,39 @@ userAuthRouter.post("/user/:user_id/fileupload", upload.single('file'), login_re
       throw new Error("현재 로그인한 사용자를 알 수 없습니다.");
     }
 
-    // path 모듈을 활용해 절대 경로를 기반으로 추가적인 상대 경로를 조합해주어야 합니다.
-    // 현재 working directory는 /back/src/routers/ 입니다.
-    // working directory의 절대 경로에다가 ../uploads/ 라는 상대 경로를 추가해줍니다.
-    // multer의 storage와 같은 경로를 사용해야 합니다.
-
+    // 사용자가 업로드한 파일이 multer storage에 저장이 되어 있으므로 이 데이터를 객체 변수에 담아 mongoose가 다룰 수 있도록 해줍니다.
+    // [주의] path 모듈을 활용해 절대 경로를 기반으로 추가적인 상대 경로를 조합해주어야 합니다.
+    //       - 현재 working directory는 /back/src/routers/ 입니다.
+    //       - working directory의 절대 경로에다가 ../uploads/ 라는 상대 경로를 추가해줍니다.
+    //       - multer의 storage와 같은 경로를 사용해야 합니다.
     const fileObject = {
       file: {
         data: fs.readFileSync(path.join(__dirname, '../uploads/') + req.file.filename),
         contentType: 'image/jpg',
       }
     }
-  
-    console.log(fileObject)
     
+    // mongoose를 사용해서 이미지 데이터 객체를 사용자 계정 document에 포함시켜줍니다.
     const uploadedFile = await UserModel.findOneAndUpdate({ id: currentUserId }, { $set: fileObject }, { returnOriginal: false });
 
     if (uploadedFile.error) {
       throw new Error(uploadedFile.error);
     }
 
-    const base64Img = await UserModel.findOne({  });
-
+    // 파일이 정상적으로 DB에 저장이 되었다면 multer storage에 보관되어 있는 임시 파일을 삭제해줍니다.
+    // 그렇지 않으면 임시 파일이 uploads 폴더에 계속 쌓이게 됩니다.
+    // fs.unlink는 callback-style API이므로 결과를 적절하게 비동기적으로 핸들링해줍니다.
+    const clearMulterBuffer = fs.unlink(path.join(__dirname, '../uploads/') + req.file.filename, function resultHandler (err) {
+      if (err) {
+          console.log("업로드된 임시 파일 삭제에 실패했습니다: ", err);
+      } else {
+          console.log("업로드된 임시 파일을 삭제했습니다.");
+      }
+    });
+    
+    // Hex 형식으로 Buffer에 들어있는 이미지 데이터를 base64 형식으로 바꿔줍니다.
+    const base64Img = new Buffer.from(fileObject["file"]["data"]).toString('base64');
+    console.log(base64Img)
     res.status(200).json(base64Img);
   }
   catch(error){
