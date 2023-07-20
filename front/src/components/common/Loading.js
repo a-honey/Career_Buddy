142d84c405e14055c41d6eda0a1294ced6016629
a-honey <jungahyeon0512@khu.ac.kr>
2023-07-20 18:11:24 +0900

    로딩페이지 div로 변경

--- front/src/components/common/Loading.js
+++ front/src/components/common/Loading.js
@@ -3,10 +3,7 @@ import { styled } from "styled-components";
 const Loading = () => {
   return (
     <LoadingBlock>
-      <img
-        src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20141118_53%2Fjess3366_1416271194923iU2Oy_PNG%2Fsddfssfd.PNG&type=sc960_832"
-        alt="로딩페이지"
-      />
+      <div>로딩중압니다!</div>
     </LoadingBlock>
   );
 };
